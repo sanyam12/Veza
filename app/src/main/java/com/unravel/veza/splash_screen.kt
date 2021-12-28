0c006ad3fd1848857865c7815c7cd1abdf9253e9
Sanyam Ratreja <sanyamratreja18@gmail.com>
2021-12-28 16:15:27 +0530

    base code

--- app/src/main/java/com/unravel/veza/splash_screen.kt
+++ app/src/main/java/com/unravel/veza/splash_screen.kt
@@ -3,9 +3,9 @@ package com.unravel.veza
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class splash_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
     }
 }
