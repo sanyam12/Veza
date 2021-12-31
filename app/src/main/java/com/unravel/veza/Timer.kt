56f003fed9162860e1b97daef6065db252ec958d
Yuvraj Singh <ysrishu1031@gmail.com>
2021-12-31 17:01:01 +0530

    initial commit

---
+++ app/src/main/java/com/unravel/veza/Timer.kt
@@ -0,0 +1,33 @@
+package com.unravel.veza
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+class Timer : AppCompatActivity() {
+    var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+        title = "POMODORO"
+    }
+    fun startTimer(view: View){
+        val countTime: TextView = findViewById(R.id.textView3)
+        object : CountDownTimer(1000 * 1800, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+             countTime.text = counter.toString()
+                counter++
+            }
+
+            override fun onFinish() {
+                countTime.text = getString(R.string.finished)
+            }
+
+        }.start()
+    }
+
+
+
+}
