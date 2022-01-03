478334e3723f0c1001bca401bcd124a930db8b58
Yuvraj Singh <ysrishu1031@gmail.com>
2022-01-03 10:49:38 +0530

    initial commit

---
+++ app/src/main/java/com/unravel/veza/Answers.kt
@@ -0,0 +1,11 @@
+package com.unravel.veza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Answers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answers)
+    }
+}
