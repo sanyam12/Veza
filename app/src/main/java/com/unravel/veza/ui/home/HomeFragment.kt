0df8ccb2d940e1dae681b8d1794a2057e963dd7c
Sanyam Ratreja <sanyamratreja18@gmail.com>
2022-01-05 19:45:13 +0530

    android commit not working
    android commit not working

--- app/src/main/java/com/unravel/veza/ui/home/HomeFragment.kt
+++ app/src/main/java/com/unravel/veza/ui/home/HomeFragment.kt
@@ -1,15 +1,22 @@
 package com.unravel.veza.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.unravel.veza.Doubts
 import com.unravel.veza.R
+import com.unravel.veza.Timer
 import com.unravel.veza.databinding.FragmentHomeBinding
+import com.unravel.veza.ui.notes.NotesFragment
+import com.unravel.veza.ui.profile.ProfileFragment
 
 class HomeFragment : Fragment() {
 
@@ -30,12 +37,43 @@ private var _binding: FragmentHomeBinding? = null
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textHome
-    homeViewModel.text.observe(viewLifecycleOwner, Observer {
-      textView.text = it
-    })
     return root
   }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        val pomo: Button = view.findViewById(R.id.imageButton2)
+//        pomo.setOnClickListener{
+//            val intent = Intent(context, Timer::class.java)
+//            startActivity(intent)
+//        }
+
+//        val notes:Button = view.findViewById(R.id.imageButton4)
+//        notes.setOnClickListener{
+//            val fragment: Fragment = NotesFragment()
+//            val transaction: FragmentTransaction? = getFragmentManager()?.beginTransaction()
+//            transaction!!.replace(R.id.notesFragment, fragment)
+//            transaction.addToBackStack(null)
+//            transaction.commit()
+//
+//        }
+
+//        val doubts: Button = view.findViewById(R.id.imageButton5)
+//        doubts.setOnClickListener{
+//            val intent = Intent(context, Doubts::class.java)
+//            startActivity(intent)
+//        }
+
+//        val profile: Button = view.findViewById(R.id.imageButton6)
+//        profile.setOnClickListener{
+//            val fragment: Fragment = ProfileFragment()
+//            val transaction: FragmentTransaction? = getFragmentManager()?.beginTransaction()
+//            transaction!!.replace(R.id.profileFragment, fragment)
+//            transaction.addToBackStack(null)
+//            transaction.commit()
+//        }
+
+    }
 
 override fun onDestroyView() {
         super.onDestroyView()
